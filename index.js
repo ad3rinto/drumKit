8c84f152809fb856f05686fa1c72e5d2596c7c5a
ad3rinto <aad3rinto@gmail.com>
2022-07-29 14:48:23 +0100

    added event listeners to the buttons

---
+++ index.js
@@ -0,0 +1,46 @@
+var drumButtons = document.querySelectorAll('.drum');
+console.log(drumButtons)
+
+for(var i = 0; i < drumButtons.length; i++ ){
+    
+    drumButtons[i].addEventListener('click', playSound);
+
+}
+
+
+// function handleClick(){
+//     alert("I got clicked");
+// }
+
+function playSound () {
+    if(this.innerHTML === 'w'){
+    
+    let audio = new Audio('/sounds/crash.mp3');
+     audio.play();
+    } 
+    else if(this.innerHTML === 'a'){
+        audio = new Audio('/sounds/kick-bass.mp3');
+        audio.play();
+    }
+    else if (this.innerHTML === 's') {
+        audio = new Audio('/sounds/snare.mp3');
+        audio.play();
+    }
+    else if (this.innerHTML === 'd') {
+        audio = new Audio('/sounds/tom-1.mp3');
+        audio.play();
+    }
+    else if (this.innerHTML === 'j') {
+        audio = new Audio('/sounds/tom-2.mp3');
+        audio.play();
+    }
+    else if (this.innerHTML === 'k') {
+        audio = new Audio('/sounds/tom-3.mp3');
+        audio.play();
+    }
+    else {
+        audio = new Audio('sounds/tom-4.mp3');
+        audio.play();
+    }
+}
+
