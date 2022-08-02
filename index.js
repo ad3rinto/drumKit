049249caf7bb7a1d7e26d7e2c4cc7ffb1518ee27
Aderinto <a127014@amerisourcebergen.com>
2022-08-02 17:32:27 +0100

    refactored code. Added function to animate buttons

--- index.js
+++ index.js
@@ -1,81 +1,71 @@
-var drumButtons = document.querySelectorAll('.drum');
-console.log(drumButtons)
 
-for(var i = 0; i < drumButtons.length; i++ ){
-    
-    drumButtons[i].addEventListener('click', function () {
-        switch (this.innerHTML) {
-            case "w":
-                var audio = new Audio('/sounds/crash.mp3');
-                audio.play();
-                break;
-            case "a":
-                audio = new Audio('/sounds/kick-bass.mp3');
-                audio.play();
-                break;
-            case "s":
-                audio = new Audio('/sounds/snare.mp3');
-                audio.play();
-                break;
-            case "d":
-                audio = new Audio('/sounds/tom-1.mp3');
-                audio.play();
-                break;
-            case "j":
-                audio = new Audio('/sounds/tom-2.mp3');
-                audio.play();
-                break;
-    
-            case "k":
-                audio = new Audio('/sounds/tom-3.mp3');
-                audio.play();
-                break;
-    
-        
-            default:
-                audio = new Audio('/sounds/tom-4.mp3');
-                audio.play();
-                break;
-                  
-        }
+// Create function to determin which sound to play
 
-        ; 
+let playSound = function (trigger) {
+    switch (trigger) {
+        case "w":
+            var crash = new Audio('/sounds/crash.mp3');
+            crash.play();
+            break;
+        case "a":
+            var kickbass = new Audio('/sounds/kick-bass.mp3');
+            kickbass.play();
+            break;
+        case "s":
+            var snare = new Audio('/sounds/snare.mp3');
+            snare.play();
+            break;
+        case "d":
+            var tom1 = new Audio('/sounds/tom-1.mp3');
+            tom1.play();
+            break;
+        case "j":
+            var tom2 = new Audio('/sounds/tom-2.mp3');
+            tom2.play();
+            break;
+
+        case "k":
+            var tom3 = new Audio('/sounds/tom-3.mp3');
+            tom3.play();
+            break;
+
+    
+        default:
+            var tom4 = new Audio('/sounds/tom-4.mp3');
+            tom4.play();
+            break;
+              
+    }
+    
 }
-); 
+
+
+// Determining which key was pressed
+
+document.addEventListener("keydown", function(e){
+    playSound(e.key);
+    animateButton(e.key);
+    }
+);
 
 // Adding function to play sounds when a drum is clicked 
 
 
-
-
-    // if(this.innerHTML === 'w'){
+var drumButtons = document.querySelectorAll('.drum');
+for(var i = 0; i < drumButtons.length; i++ ){
+    drumButtons[i].addEventListener('click', function(){
+        var but = this.innerHTML;
+        playSound(but);
+        animateButton(but);
+    });
     
-    // let audio = new Audio('/sounds/crash.mp3');
-    //  audio.play();
-    // } 
-    // else if(this.innerHTML === 'a'){
-    //     audio = new Audio('/sounds/kick-bass.mp3');
-    //     audio.play();
-    // }
-    // else if (this.innerHTML === 's') {
-    //     audio = new Audio('/sounds/snare.mp3');
-    //     audio.play();
-    // }
-    // else if (this.innerHTML === 'd') {
-    //     audio = new Audio('/sounds/tom-1.mp3');
-    //     audio.play();
-    // }
-    // else if (this.innerHTML === 'j') {
-    //     audio = new Audio('/sounds/tom-2.mp3');
-    //     audio.play();
-    // }
-    // else if (this.innerHTML === 'k') {
-    //     audio = new Audio('/sounds/tom-3.mp3');
-    //     audio.play();
-    // }
-    // else {
-    //     audio = new Audio('sounds/tom-4.mp3');
-    //     audio.play();
-    // }
-}
+}   
+    
+//  Function to add animation to pressed keys  
 
+function animateButton(currentKey){
+    var activeKey = document.querySelector("."+ currentKey);
+    activeKey.classList.add("pressed");
+
+
+};
